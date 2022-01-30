dc77ae467706e02c97541c37bdb33a03a2d9051b
rodevhit <rodevhit@gmail.com>
2022-01-30 18:04:28 +0530

    https-only cookie added, token validation with redis added

--- Controllers/Auth.Controller.js
+++ Controllers/Auth.Controller.js
@@ -1,133 +1,176 @@
 const createError = require('http-errors')
-const { authSchema } = require('../helpers/validation_schema')
-const {signAccessToken, signRefreshToken, verifyRefreshToken} = require('../helpers/jwt_helper')
+const { loginSchema, regSchema } = require('../helpers/validation_schema')
+const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../helpers/jwt_helper')
 const client = require('../helpers/init_redis')
 const passHelper = require('../helpers/passHelper')
 
 //load models
 const User = require('../Models/User.model')
 
-
-
-
 module.exports = {
 
-    login: async (req, res, next) => {
-        try {
-                const result = await authSchema.validateAsync(req.body)
-                const user = await User.findOne({
-                    attributes:['user_id', 'user_password'],
-                    where: {
-                        user_name: result.username,
-                        user_type: result.type,
-                        user_status: 1
-                    },
-                    raw:true
-                })
-
-                
-                if (!user) throw createError.NotFound('User not registered')
-                const finalNodeGeneratedHash = user.user_password.replace('$2y$', '$2b$');
-
-                const isMatch = await passHelper.isValidPassword(result.password, finalNodeGeneratedHash)
-                if (!isMatch)
-                    throw createError.Unauthorized('Invalid Username/Password')
+	login: async (req, res, next) => {
+		try {
+			const result = await loginSchema.validateAsync(req.body)
+			const user = await User.findOne({
+				attributes: ['user_id', 'user_password'],
+				where: {
+					user_name: result.username,
+					user_status: 1
+				},
+				raw: true
+			})
 
 
-                const accessToken = await signAccessToken(user.user_id)
-                const refToken = await signRefreshToken(user.user_id)
+			if (!user) throw createError.NotFound('User not registered');
 
-                res.send({ accessToken: accessToken, refreshToken: refToken })
-
-        } catch (error) {
-          if (error.isJoi === true)
-            return next(createError.BadRequest('Invalid Username/Password'))
-            next(error)
-        }
-    },
+			const isMatch = await passHelper.isValidPassword(result.password, user.user_password)
+			if (!isMatch)
+				throw createError.Unauthorized('Invalid Username/Password')
 
 
-    refreshToken: async (req, res, next) => {
-        try {
-            const { refreshToken } = req.body
-            console.log(refreshToken)
-            if (!refreshToken) throw createError.BadRequest()
-            const userId = await verifyRefreshToken(refreshToken)
+			const accessToken = await signAccessToken(user.user_id)
+			const refreshToken = await signRefreshToken(user.user_id)
 
-            const accessToken = await signAccessToken(userId)
-            const refToken = await signRefreshToken(userId)
-            res.send({ accessToken: accessToken, refreshToken: refToken })
-        } catch (error) {
-          next(error)
-        }
-    },
-
-
-    register: async (req, res, next) => {
-        try {
-
-            const { refreshToken } = req.body
-            if (!refreshToken) throw createError.BadRequest()
-                const userId = await verifyRefreshToken(refreshToken)
-            if (!userId) throw createError.Unauthorized('Refresh Token not valid')
-
-
-            const result = await authSchema.validateAsync(req.body)  
-            const doesExist = await User.findOne({
-              where: { 
-                user_name: result.username
-              } 
-            });
-
-            if (doesExist){
-                throw createError.Conflict(`${result.username} is already been registered.`)
+			let makeSecure = false
+            if (process.env.COOKIE_SAME_SITE == 'none') {
+                makeSecure = true
             }
 
-            const hashPassword = await passHelper.hashPassword(result.password)
-
-            const savedUser = await User.create({
-                user_name: req.body.username,
-                user_password: hashPassword,
-                user_type: req.body.type
-            })
-            
-            res.send({
-              status: 200,
-              message: 'Registration successfull',
-              data: savedUser
+            res.status(202).cookie('secretCookie', accessToken, {
+                sameSite: process.env.COOKIE_SAME_SITE,
+                path: '/',
+                expires: new Date(new Date().getTime() + process.env.ACCESS_TOKEN_EXPIRY * 1000),
+                httpOnly: true,
+                domain: process.env.COOKIE_DOMAIN,
+                secure: makeSecure
+            });
+            res.status(202).cookie('secretCook', refreshToken, {
+                sameSite: process.env.COOKIE_SAME_SITE,
+                path: '/',
+                expires: new Date(new Date().getTime() + process.env.REFRESH_TOKEN_EXPIRY * 1000),
+                httpOnly: true,
+                domain: process.env.COOKIE_DOMAIN,
+                secure: makeSecure
             });
 
-        } catch (error) {
-              if (error.isJoi === true) error.status = 422
-              next(error)
-        }
-    },
+			res.send({ 
+				'status': 200,
+				'message': 'Login successfull'
+			})
 
+		} catch (error) {
+			if (error.isJoi === true)
+				return next(createError.BadRequest('Invalid Username/Password'))
+			next(error)
+		}
+	},
 
+	refreshToken: async (req, res, next) => {
+		try {
+			if (req.cookies.secretCook == '') throw createError.BadRequest('Refresh token is required to generate new access token.');
+            let refreshToken = '';
+            if (req.body.refreshToken) {
+                refreshToken = req.body.refreshToken;
+            }
 
+            if (req.cookies.secretCook) {
+                refreshToken = req.cookies.secretCook
+            }
+            const userId = await verifyRefreshToken(refreshToken);
+            if (!userId) throw createError.BadRequest('Invalid Refresh token or it has been expired, try to Login!')
 
+            const newAccessToken = await signAccessToken(userId);
+            const newRefreshToken = await signRefreshToken(userId);
 
+            let makeSecure = false
+            if (process.env.COOKIE_SAME_SITE == 'none') {
+                makeSecure = true
+            }
 
+            res.status(202).cookie('secretCookie', newAccessToken, {
+                sameSite: process.env.COOKIE_SAME_SITE,
+                path: '/',
+                expires: new Date(new Date().getTime() + process.env.ACCESS_TOKEN_EXPIRY * 1000),
+                httpOnly: true,
+                domain: process.env.COOKIE_DOMAIN,
+                secure: makeSecure
+            });
+            res.status(202).cookie('secretCook', newRefreshToken, {
+                sameSite: process.env.COOKIE_SAME_SITE,
+                path: '/',
+                expires: new Date(new Date().getTime() + process.env.REFRESH_TOKEN_EXPIRY * 1000),
+                httpOnly: true,
+                domain: process.env.COOKIE_DOMAIN,
+                secure: makeSecure
+            });
+            return res.send({
+                'status': 200,
+                'message': 'New Access Token generated successfully.',
+            });
+		} catch (error) {
+			next(error)
+		}
+	},
 
+	register: async (req, res, next) => {
+		try {
+			const result = await regSchema.validateAsync(req.body)
+			const doesExist = await User.findOne({
+				where: {
+					user_name: result.username
+				}
+			});
 
+			if (doesExist) {
+				throw createError.Conflict(`${result.username} is already been registered.`)
+			}
 
-  logout: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body
-      if (!refreshToken) throw createError.BadRequest()
-      const userId = await verifyRefreshToken(refreshToken)
-      client.DEL(userId, (err, val) => {
-        if (err) {
-          console.log(err.message)
-          throw createError.InternalServerError()
-        }
-        console.log(val)
-        res.sendStatus(204)
-      })
-    } catch (error) {
-      next(error)
-    }
-  },
+			const hashPassword = await passHelper.hashPassword(result.password)
+
+			const savedUser = await User.create({
+				user_name: req.body.username,
+				user_password: hashPassword,
+				user_type: req.body.type
+			})
+
+			res.send({
+				status: 200,
+				message: 'Registration successfull',
+				data: savedUser
+			});
+
+		} catch (error) {
+			if (error.isJoi === true) error.status = 422
+			next(error)
+		}
+	},
+
+	logout: async (req, res, next) => {
+		try {
+			if (req.body.refreshToken == '' || req.cookies.secretCook == '') throw createError.BadRequest('Alert!! something unexpected happened, contact admin.');
+            let refreshToken = '';
+            if (req.body.refreshToken) {
+                refreshToken = req.body.refreshToken;
+            }
+
+            if (req.cookies.secretCook) {
+                refreshToken = req.cookies.secretCook;
+            }
+            res.clearCookie('secretCookie', { domain: process.env.COOKIE_DOMAIN });
+            res.clearCookie('secretCook', { domain: process.env.COOKIE_DOMAIN });
+			const userId = await verifyRefreshToken(refreshToken)
+
+			client.DEL(`auth-${userId}`, (err, val) => {
+				if (err) {
+					throw createError.InternalServerError()
+				}
+				res.sendStatus(204)
+			})
+		} catch (error) {
+			next(error)
+		}
+	},
 
 
 }
--- helpers/validation_schema.js
+++ helpers/validation_schema.js
@@ -1,7 +1,6 @@
 const Joi = require('@hapi/joi')
 
-const authSchema = Joi.object({
-  // username: Joi.string().email().lowercase().required(),
+const regSchema = Joi.object({
   username: Joi.string().regex(/^([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})|([A-Za-z0-9._%\+\-]+@[a-z0-9.\-]+\.[a-z]{2,3})$/).required().messages({
       "string.base": `"username" should be a type of 'text'`,
       "string.empty": `"username" cannot be an empty field`,
@@ -12,7 +11,18 @@ const authSchema = Joi.object({
   type: Joi.number().min(1).required(),
   password: Joi.string().min(8).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 })
+const loginSchema = Joi.object({
+  username: Joi.string().regex(/^([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})|([A-Za-z0-9._%\+\-]+@[a-z0-9.\-]+\.[a-z]{2,3})$/).required().messages({
+      "string.base": `"username" should be a type of 'text'`,
+      "string.empty": `"username" cannot be an empty field`,
+      "string.min": `"username" should have a minimum length of {#limit}`,
+      "string.max": `"username" should have a maximum length of {#limit}`,
+      "any.required": `"username" is a required field`
+    }),
+  password: Joi.string().min(8).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+})
 
 module.exports = {
-  authSchema,
+  regSchema,
+  loginSchema
 }
